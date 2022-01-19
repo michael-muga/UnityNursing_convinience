1832fdf70cf3b93933b616bae565a4004aba037a
saretotracy <tracysareto@gmail.com>
2022-01-19 15:06:09 +0300

    modifications

--- train.js
+++ train.js
@@ -1,13 +1,22 @@
+let intro = document.querySelector(".introduction")
+let intro_play = document.querySelector(".intro-play")
 let hallu_play = document.querySelector(".hallucination")
 let tutorial = document.querySelector(".tutorial")
 let close_btn = document.querySelector(".close")
+
 
 hallu_play.addEventListener("click", () => {
     tutorial.classList.remove("hide")
 
 });
 
+intro_play.addEventListener("click", () => {
+    intro.classList.remove("hide")
+
+});
+
 close_btn.addEventListener("click", () => {
     tutorial.classList.add("hide")
+    intro.classList.add("hide")
 
 });
