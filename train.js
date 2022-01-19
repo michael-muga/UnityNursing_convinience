52decc06cce45bf91f7db774c80eefbd5878c485
saretotracy <tracysareto@gmail.com>
2022-01-20 00:40:41 +0300

    event listners

--- train.js
+++ train.js
@@ -7,17 +7,21 @@ let close_btn = document.querySelector(".close")
 
 intro_play.addEventListener("click", () => {
     intro.classList.remove("hide")
+    tutorial.classList.add("hide")
 
 });
 
 
 hallu_play.addEventListener("click", () => {
     tutorial.classList.remove("hide")
+    intro.classList.add("hide")
 
 });
 
 close_btn.addEventListener("click", () => {
-    tutorial.classList.add("hide")
     intro.classList.add("hide")
+});
 
+close_btn.addEventListener("click", () => {
+    tutorial.classList.add("hide")
 });
