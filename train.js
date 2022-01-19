fd7cf4e7fd074b98f0213684ce0c2bccf748d328
saretotracy <tracysareto@gmail.com>
2022-01-19 14:34:27 +0300

    add click eventlistner to hallucination play button

---
+++ train.js
@@ -0,0 +1,8 @@
+let hallu_play = document.querySelector(".hallucination")
+let tutorial = document.querySelector(".tutorial")
+let close_btn = document.querySelector(".close")
+
+hallu_play.addEventListener("click", () => {
+    tutorial.classList.remove("hide")
+
+});
